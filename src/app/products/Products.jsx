ec3b2811bdc69387bde4251346789ecbc19a6ca1
Piotr <piotr.osiak@nekk.pl>
2022-04-11 23:31:24 +0200

    add filtering products

--- src/app/products/Products.jsx
+++ src/app/products/Products.jsx
@@ -12,8 +12,6 @@ import { ProductsPagination } from '../../components/Pagination/ProductsPaginati
 import axios from 'axios';
 
 const ProductItemModal = (props) => { 
-  // console.log(props);
-
   return (
     <div id={`product_${props.id}`} className={`product__modal ${props.show ? 'product__modal--show' : ''}`} data-product={props.id}>
       <div className="product__modal-overlay">
@@ -97,10 +95,8 @@ const Spinner = () => {
 };
 
 const ProductContainer = (props) => {
-  const [products, setProducts] = useState({items: [], meta: []});
   const [dataModal, setDataModal] = useState(null);
   const [openModal, setOpenModal] = useState(false);
-  const [loading, setLoading] = useState(true);
 
   const handleProductDetails = (e, data) => {   
     e.preventDefault();
@@ -114,87 +110,118 @@ const ProductContainer = (props) => {
     setOpenModal(false);
 
     return openModal;
-  };
+  }; 
+
+  return props.loading ? <Spinner /> : props.items.length > 0 ? (
+    <>
+      <div className="products">
+        {
+          props.items.map(item => {
+            return (
+              <ProductItem 
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                description={item.description}
+                rating={item.rating}
+                image={item.image}
+                promo={item.promo}
+                active={item.active}
+                onClick={evt => 
+                  handleProductDetails(evt, item)
+                }
+              />
+            )
+          })
+        }
+      </div>
+      {
+        dataModal && (
+          <ProductItemModal 
+            id={dataModal[0].id} 
+            image={dataModal[0].image} 
+            name={dataModal[0].name} 
+            description={dataModal[0].description} 
+            show={dataModal} 
+            handleClose={handleCloseModal} 
+          />
+        )
+      }
+      <ProductsPagination pages={props.totalPages} />
+    </>
+  ) : <ProductsNotFound />
+};
+
+export const Products = () => {
+  const [products, setProducts] = useState({items: [], meta: []});
+  const [loading, setLoading] = useState(true);  
+
+  const [isActive, setIsActive] = useState(false);
+  const [isPromo, setIsPromo] = useState(false);
+  
+  const checkboxActive = () => setIsActive(isActive => !isActive);
+  const checkboxPromo = () => setIsPromo(isPromo => !isPromo);
 
   useEffect(() => { 
     let mounted = true;
 
+    setLoading(true);
+
     axios.get('https://join-tsh-api-staging.herokuapp.com/products?limit=6&page=1').then(response => {
-      // console.log(response.data)
       if(mounted) {
         setLoading(false);
 
-        
+        let items = [];
 
-        // setProducts(s => ({...s, items: response.data.items, meta: response.data.meta}));
+        if(isActive) {                   
+          response.data.items.filter(item => item.active === isActive).map(item => {
+            items.push(item);
+          });               
+          
+          if(items.length) {
+            setProducts(s => ({...s, items: items, meta: response.data.meta}))
+          }
+        } else if (isPromo) {
+          response.data.items.filter(item => item.promo === isPromo).map(item => {
+            items.push(item);
+          });               
+          
+          if(items.length) {
+            setProducts(s => ({...s, items: items, meta: response.data.meta}))
+          }
+        } else if (isActive && isPromo) {
+          response.data.items.filter(item => item.active === isActive && item.promo === isPromo).map(item => {
+            items.push(item);
+          });               
+          
+          if(items.length) {
+            setProducts(s => ({...s, items: items, meta: response.data.meta}))
+          }
+        } else {
+          console.log('not active')        
+          setProducts(s => ({...s, items: response.data.items, meta: response.data.meta}))  
+        }
       }
     }).catch(error => {});
 
     return function cleanup() {
       mounted = false;
     }
-  }, []);  
+  }, [isActive, isPromo]);  
 
-  return loading ? <Spinner /> :
-  (       
-    products.items.length > 0 ? 
-      (
-        <>
-          <div className='products'>
-            {
-              products.items.map(product => {
-                return (
-                  <ProductItem 
-                    key={product.id}
-                    id={product.id}
-                    name={product.name}
-                    description={product.description}
-                    rating={product.rating}
-                    image={product.image}
-                    promo={product.promo}
-                    active={product.active}
-                    onClick={evt => 
-                      handleProductDetails(evt, product)
-                    }
-                  />
-                )
-              })
-            }
-          </div>    
-          {
-            dataModal && (
-              <ProductItemModal 
-                id={dataModal[0].id} 
-                image={dataModal[0].image} 
-                name={dataModal[0].name} 
-                description={dataModal[0].description} 
-                show={openModal} 
-                handleClose={handleCloseModal} 
-              />
-            )
-          }
-          <ProductsPagination pages={products.meta.totalPages} />
-        </>
-      ) : <ProductsNotFound />
-  )
-};
-
-export const Products = () => {
-  const [isActive, setIsActive] = useState(true);
-  const [isPromo, setIsPromo] = useState(true);
-
-  const checkboxActive = () => setIsActive(isActive => !isActive);
-  const checkboxPromo = () => setIsPromo(isPromo => !isPromo);
 
   return (
     <>
       <Header active={checkboxActive} promo={checkboxPromo} />
 
       <div className="container">
-        <ProductContainer showActive={isActive} showPromo={isPromo} />
+        <ProductContainer           
+          loading={loading}
+          items={products.items}
+          totalPages={products.meta.totalPages}
+        />
 
         {/* <h2>Products page</h2> */}
-        {/* <p>Products: { products.length }</p> */}
         {/* <Link to={AppRoute.login}> Login </Link> */}
       </div>
     </>
