4d57a1793ccff95b04452f57c670b41a9e5be921
Piotr <piotr.osiak@nekk.pl>
2022-04-20 18:43:44 +0200

    start login form

--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import { AppRoute } from '../../routing/AppRoute.enum';
 
 export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+
+  const validate = () => username.length > 0 && password.length > 0;
+  
+  const handleLogin = (e) => {
+    e.preventDefault();
+    return false;
+  };
+
   return (
     <>
       <Link to={AppRoute.home}>Products page</Link>
@@ -12,16 +24,16 @@ export const Login = () => {
         <div>
           <label>
             username:
-            <input name="username" />
+            <input autoFocus={true} name="username" onChange={(e) => {setUsername(e.target.value) }} />
           </label>
         </div>
         <div>
           <label>
             password:
-            <input name="password" type="password" />
+            <input name="password" type="password" onChange={(e) => {setPassword(e.target.value)}} />
           </label>
         </div>
-        <button type="submit">submit</button>
+        <button type="submit" onSubmit={(e) => handleLogin(e) } disabled={!validate()}>submit</button>
       </form>
     </>
   );
